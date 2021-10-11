4c72da46e3f084068ab982217fce6906a164c9da
Lisa Cormier <lcormier5@hotmail.com>
2021-10-11 18:04:24 +0000

    initial commit

---
+++ main.rb
@@ -0,0 +1,11 @@
+# class1 = players, includes lives
+# will contain methods to lose lives
+# lives, player name
+
+# class2 = math questions, includes answers
+# will contain methods to create questions and determine if the questions are right or wrong
+
+# class3 = game loop
+# method to call questions and players and replay
+# will determine if player wins or loses
+# will contain user I/O
